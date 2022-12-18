f58546ee4503b8cf8f69a214a8f9abed9fdec456
Shivansh-Mittal <shivanshmittal1444@gmail.com>
2022-12-18 14:28:11 +0530

    added contact form

--- script.js
+++ script.js
@@ -22,3 +22,20 @@ function openmenu(){
 function closemenu(){
     sidemenu.style.right = "-200px";
 }
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwzMM8W4dkmesjTdyPTBit-1xb7mF08MoBN-L5lP_i7EVt3jlMre-4bVwr37KmyefZj/exec';
+const form = document.forms['submit-to-google-sheet'];
+const msg = document.getElementById("msg");
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML = "Message Sent Successfully";
+        setTimeout(function(){
+            msg.innerHTML = ""
+        }, 7000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
